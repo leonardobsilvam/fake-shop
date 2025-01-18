40c9b35c7db12debfb5c330c3009bcdaa77460aa
leonardobsilvam <leonardobsilvam@gmail.com>
2025-01-18 10:26:08 -0300

    Desafio DevOps

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,3 +1,6 @@
+#Arquivo shel para fazer o start do container
 #!/bin/bash
+#Executa a migration do banco de dados
 python -m flask db upgrade
+#Cria as tabelas no DB e executa a aplicacao
 python -m gunicorn --bind 0.0.0.0:5000 index:app
